f60c1691b553cf2d21d9bd9e8a634054d1eb193a
Niclas Overby <niclas@overby.me>
2023-10-05 10:53:27 +0200

    Use CJS for minimal friction

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,11 @@
-import { tgzUtils } from "@yarnpkg/core";
-import fs from "fs";
+const { tgzUtils } = require("@yarnpkg/core");
+const fs = require("fs");
 
-
-const cli = async ({ name, input, output }) => {
+const tgzToZip = async ({ name, input, output }) => {
   console.log({ name, input, output })
   const tgzBuffer = fs.readFileSync(input);
   const zip = await tgzUtils.convertToZip(tgzBuffer, { stripComponents: 1, prefixPath: `node_modules/${name}` });
   fs.writeFileSync(output, zip.getBufferAndClose())
 }
 
-
-cli({ name: process.argv[2], input: process.argv[3], output: process.argv[4] })
+module.exports = { tgzToZip };
