90480b64a5d01fcd85ad91ad39d927f0e193ae2f
Niclas Overby <niclas@overby.me>
2024-12-20 15:48:14 +0100

    Add fetch-patch support

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
-const { tgzUtils, structUtils } = require("@yarnpkg/core");
+const { tgzUtils, structUtils,Configuration, Project, Cache, ThrowReport, } = require("@yarnpkg/core");
+const { PatchFetcher } = require("@yarnpkg/plugin-patch");
+const { getPluginConfiguration } = require("@yarnpkg/cli");
 const fs = require("fs");
 
 const tgzToZip = async ({ name, input, output, compressionLevel }) => {
@@ -12,4 +14,19 @@ const getLocatorHash = ({ locator }) => {
   console.log(structUtils.parseLocator(locator).locatorHash);
 }
 
-module.exports = { tgzToZip, getLocatorHash };
+const fetchPatch = async ({ locator, output }) => {
+  const patchFetcher = new PatchFetcher()
+  const locatorObj =  structUtils.parseLocator(locator)
+
+  const configuration = await Configuration.find(process.cwd(), getPluginConfiguration());
+  const {project} = await Project.find(configuration, process.cwd());
+  const cache = await Cache.find(configuration);
+  const fetcher = configuration.makeFetcher();
+  const opts = {project, fetcher, cache, checksums: project.storedChecksums, report: new ThrowReport(), cacheOptions: {skipIntegrityCheck: true}};
+
+  const outpuBuffer = (await patchFetcher.patchPackage(locatorObj, opts)).getBufferAndClose();
+  fs.writeFileSync(output, outpuBuffer)
+}
+
+
+module.exports = { tgzToZip, getLocatorHash, fetchPatch };
